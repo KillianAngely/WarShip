3721120bed0ade9964fa19313660b9f00930c854
Killian ANGELY <killian.angely@outlook.fr>
2024-06-07 12:11:57 +0200

    choose mode game

--- WarShip/ManageGameController.swift
+++ WarShip/ManageGameController.swift
@@ -8,10 +8,35 @@
 import Foundation
 import UIKit
 
-class ManageGameController : UIViewController {
-    var gamedata : [String:String]?
+class ManageGameController: UIViewController {
+    var gamedata: [String: String]?
     
     override func viewDidLoad() {
-        print(gamedata)
+        super.viewDidLoad()
+        
+        // Utilisation de if let pour un déballage optionnel sécurisé
+        guard let gameMode = gamedata?["gameMode"] else {
+            // game is not set
+            return
+        }
+        if(gameMode == "IA"){
+            //placement random next vue
+        }
+        if(gameMode == "1V1"){
+            //placement random manuel
+        }
+        
+        
+        guard let difficulty = gamedata?["difficulty"] else {
+            // difficulty is not set
+            return
+        }
+        if(difficulty == "EASY"){
+            //create a grid 6 by 6
+        }
+        if(difficulty == "HARD"){
+            //create a grid 9 by 9
+        }
+        
     }
-}
+    }
