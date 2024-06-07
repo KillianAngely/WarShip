96e74e38ca048df3b7bb3e19df92c03a3f87c449
Killian ANGELY <killian.angely@outlook.fr>
2024-06-07 11:28:28 +0200

    push game instanciate controller

---
+++ WarShip/ManageGameController.swift
@@ -0,0 +1,13 @@
+//
+//  ManageGameController.swift
+//  WarShip
+//
+//  Created by killian on 07/06/2024.
+//
+
+import Foundation
+import UIKit
+
+class ManageGameController : UIViewController {
+    
+}
---
+++ WarShip/SetupGameController.swift
@@ -0,0 +1,36 @@
+//
+//  SetupGameController.swift
+//  WarShip
+//
+//  Created by killian on 07/06/2024.
+//
+
+import Foundation
+import UIKit
+
+class SetupGameController: UIViewController{
+    var gameMode = "IA"
+    var difficulty = "EASY"
+    
+    @IBAction func createGame(_ sender: Any){
+        if(chooseGameMode.selectedSegmentIndex == 0){
+            gameMode = "IA"
+        }else{
+            gameMode = "1V1"
+        }
+        if(chooseDifficultyMode.selectedSegmentIndex == 0){
+            difficulty = "EASY"
+        }else{
+            difficulty = "HARD"
+        }
+        var gameConfig = [
+            "gameMode": gameMode,
+            "difficulty":difficulty
+        ]
+        var gameScreen = (self.storyboard?.instantiateViewController(withIdentifier: "GameController")) as! ManageGameController
+        self.navigationController?.pushViewController(gameScreen, animated:true)
+    
+    }
+    @IBOutlet weak var chooseGameMode: UISegmentedControl!
+    @IBOutlet weak var chooseDifficultyMode: UISegmentedControl!
+}
