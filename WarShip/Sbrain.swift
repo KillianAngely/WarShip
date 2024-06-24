64c23a7ce6e504597da32bf2685283fbd3362d47
Killian ANGELY <killian.angely@outlook.fr>
2024-06-24 17:38:04 +0200

    refacto real ship

--- WarShip/Sbrain.swift
+++ WarShip/Sbrain.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 func chooseRandomCase(gridSize: Int) -> String {
     let row: [Int]
     let column: [String]
@@ -27,4 +26,45 @@ func chooseRandomCase(gridSize: Int) -> String {
     return "\(columnChoose)\(rowChoose)"
 }
 
-
+func generateShip(gridSize: Int) -> [String: [String]] {
+    let directions = ["horizontal", "vertical"]
+    let lengths = [2, 3]
+    let rows = Array(0..<gridSize)
+    let columns = (0..<gridSize).map { String(UnicodeScalar("A".unicodeScalars.first!.value + UInt32($0))!) }
+    
+    var shipCoordinates = [String]()
+    var shipPlaced = false
+    
+    while !shipPlaced {
+        guard let startRow = rows.randomElement(), let startColumn = columns.randomElement(), let direction = directions.randomElement(), let length = lengths.randomElement() else {
+            continue
+        }
+        
+        shipCoordinates.removeAll()
+        var isValidPlacement = true
+        
+        for i in 0..<length {
+            if direction == "horizontal" {
+                if let colIndex = columns.firstIndex(of: startColumn), colIndex + i < gridSize {
+                    shipCoordinates.append("\(columns[colIndex + i])\(startRow)")
+                } else {
+                    isValidPlacement = false
+                    break
+                }
+            } else {
+                if startRow + i < gridSize {
+                    shipCoordinates.append("\(startColumn)\(startRow + i)")
+                } else {
+                    isValidPlacement = false
+                    break
+                }
+            }
+        }
+        
+        if isValidPlacement {
+            shipPlaced = true
+        }
+    }
+    
+    return ["ship": shipCoordinates]
+}
